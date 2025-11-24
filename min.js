4b040373589fff1c368e6d8e5f54f83294c68d51
aaajiao <aaajiao@gmail.com>
2025-11-24 19:33:50 +0100

    Refactor cursor drawing and history tracking

--- min.js
+++ min.js
@@ -1,75 +1,71 @@
-function drawCursor(mouseX, mouseY){
-    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg'); 
-    svg.setAttribute("width", "12");
-    svg.setAttribute("height", "19");
-    svg.setAttribute("viewbox", "0 0 12 19");
+(function(window) {
+    'use strict';
 
-    //part one
-    line_one = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-    line_one.setAttribute('fill-rule','evenodd');
-    line_one.setAttribute('d', 'M0 16.015V0L11.591 11.619H4.81L4.399 11.743L0 16.015Z');
-    line_one.setAttribute('fill', 'white');
-    svg.appendChild( line_one );
+    const cursorTemplate = (function createCursorTemplate() {
+        const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+        svg.setAttribute("width", "12");
+        svg.setAttribute("height", "19");
+        svg.setAttribute("viewBox", "0 0 12 19");
+        svg.style.position = "absolute";
+        svg.style.zIndex = "99999";
+        svg.style.pointerEvents = "none";
 
-    //part-two
-    line_two = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-    line_two.setAttribute('fill-rule','evenodd');
-    line_two.setAttribute('d', 'M9.08449 16.6893L5.47949 18.2243L0.797485 7.13531L4.48349 5.58231L9.08449 16.6893Z');
-    line_two.setAttribute('fill', 'white');
-    svg.appendChild( line_two );
+        const paths = [
+            { d: 'M0 16.015V0L11.591 11.619H4.81L4.399 11.743L0 16.015Z', fill: 'white' },
+            { d: 'M9.08449 16.6893L5.47949 18.2243L0.797485 7.13531L4.48349 5.58231L9.08449 16.6893Z', fill: 'white' },
+            { d: 'M7.75101 16.0086L5.90701 16.7826L2.80701 9.40861L4.64801 8.63361L7.75101 16.0086Z', fill: 'black' },
+            { d: 'M1 2.4071V13.5951L3.969 10.7291L4.397 10.5901H9.165L1 2.4071Z', fill: 'black' }
+        ];
 
-    //part-three
-    line_three = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-    line_three.setAttribute('fill-rule','evenodd');
-    line_three.setAttribute('d', 'M7.75101 16.0086L5.90701 16.7826L2.80701 9.40861L4.64801 8.63361L7.75101 16.0086Z');
-    line_three.setAttribute('fill', 'black');
-    svg.appendChild( line_three );
+        paths.forEach(p => {
+            const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+            path.setAttribute('fill-rule', 'evenodd');
+            path.setAttribute('d', p.d);
+            path.setAttribute('fill', p.fill);
+            svg.appendChild(path);
+        });
 
-    //part-four
-    line_four = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-    line_four.setAttribute('fill-rule','evenodd');
-    line_four.setAttribute('d', 'M1 2.4071V13.5951L3.969 10.7291L4.397 10.5901H9.165L1 2.4071Z');
-    line_four.setAttribute('fill', 'black');
-    svg.appendChild( line_four );
+        return svg;
+    })();
 
-    svg.style.position = "absolute"; 
-    svg.style.left = mouseX + "px"; 
-    svg.style.top = mouseY + "px"; 
-    svg.style.zIndex = 99999; 
-    document.body.appendChild(svg);
-}
-
-function writeLocation(posX, posY){
-    var cursorList = [];
-    if(!sessionStorage.cursorList){
-        console.log("don't have history");
-        cursorList.push({x:posX,y:posY});
-        sessionStorage.cursorList = JSON.stringify(cursorList);
-    }else{
-        const temp = sessionStorage.cursorList;
-        cursorList = JSON.parse(temp);
-        cursorList.push({x:posX,y:posY});
-        sessionStorage.cursorList = JSON.stringify(cursorList);
+    function drawCursor(x, y) {
+        const svg = cursorTemplate.cloneNode(true);
+        svg.style.left = x + "px";
+        svg.style.top = y + "px";
+        document.body.appendChild(svg);
     }
-}
 
-function drawList(){
-    if(sessionStorage.cursorList){
-        const cursorList = JSON.parse (sessionStorage.cursorList);
-        if(cursorList){
-            cursorList.map((item)=>{
-                drawCursor(item.x,item.y);
-            })
+    function getHistory() {
+        try {
+            return JSON.parse(sessionStorage.getItem('cursor-traces-list')) || [];
+        } catch (e) {
+            return [];
         }
     }
-}
 
-function findALabel(startNode) {
-    if (!(startNode instanceof HTMLElement)) return false;
-    if (startNode.nodeName === "HTML") return false;
-    if (startNode.nodeName==="A"){
-        return true;
-    }else{
-        return findALabel(startNode.parentElement);
+    function recordPosition(x, y) {
+        const list = getHistory();
+        list.push({ x, y });
+        sessionStorage.setItem('cursor-traces-list', JSON.stringify(list));
     }
-}
+
+    function init() {
+        console.log("cursor-traces (browser) initialized!");
+        const history = getHistory();
+        history.forEach(item => drawCursor(item.x, item.y));
+
+        document.addEventListener('click', (e) => {
+            const link = e.target.closest('a');
+            if (link) {
+                recordPosition(e.pageX, e.pageY);
+                drawCursor(e.pageX, e.pageY);
+            }
+        });
+    }
+
+    // 暴露给全局对象
+    window.CursorTraces = {
+        startCursorTraces: init
+    };
+
+})(window);
